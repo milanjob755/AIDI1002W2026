a50ea0e5718acbbcc284182c984a2438509813cd
milanjob755 <82503204+milanjob755@users.noreply.github.com>
2026-01-06 14:25:09 -0500

    created some variables
    created some variables and messing with it

---
+++ Module 1/Activity2.py
@@ -0,0 +1,15 @@
+# STEP 1: Create different data types
+numbers = [10,20,30,50]     # list
+student = {"name":"milan","age":30} # dictionary
+grades =   {80,45,60}          # tuple
+message = "Welcome to the Machine learning" # string
+ 
+# STEP 2: Print data types
+print(type(numbers))
+print(type(student))
+print(type(grades))
+print(type(message))
+ 
+# STEP 3: Access list and dictionary elements
+print(numbers[0])         # first item in list
+print(student["name"])   # get value from dictionary
